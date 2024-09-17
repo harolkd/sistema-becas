54bbdd99f1f6ab752f14775cebbddf6232118e94
harolkd <85809507+harolkd@users.noreply.github.com>
2024-09-17 18:19:11 -0500

    añadidas funciones de notas

--- forms.py
+++ forms.py
@@ -1,7 +1,22 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, SubmitField
+from wtforms import StringField, PasswordField, SubmitField, IntegerField, FloatField
 
 class AñadirEstudianteForm(FlaskForm):
     username = StringField('Usuario')
     password = PasswordField('Contraseña')
     submit = SubmitField('Añadir')
+
+class EliminarEstudianteForm(FlaskForm):
+    username = StringField('Usuario')
+    submit = SubmitField('Eliminar')
+
+class AñadirNotaForm(FlaskForm):
+    name = StringField('Nombre')
+    credits = IntegerField('Creditos')
+    nota = FloatField('Nota')
+    submit = SubmitField('Añadir')
+
+class EliminarNotaForm(FlaskForm):
+    username = StringField('Usuario')
+    name = StringField('Nombre')
+    submit = SubmitField('Eliminar')
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, redirect
-from forms import AñadirEstudianteForm
+from forms import AñadirEstudianteForm, EliminarEstudianteForm, AñadirNotaForm, EliminarNotaForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'robtop'
@@ -20,19 +20,45 @@ def main():
 def admin():
     return render_template('admin/index.html')
 
-@app.route("/eliminar_estudiante")
-def eliminar_estudiante():
-    return render_template('admin/eliminar_estudiante/eliminar_estudiante.html')
-
 @app.route("/añadir_estudiante", methods=['GET', 'POST'])
 def añadir_estudiante():
     form = AñadirEstudianteForm()
     if form.is_submitted():
         # logica
-        print("BIEEN UN LOGIN")
-
-        return redirect(url_for('admin'))
+        # form.password.data
+        # form.username.data
+        return redirect(url_for('añadir_estudiante'))
     return render_template('admin/añadir_estudiante/añadir_estudiante.html', form=form)
+
+@app.route("/añadir_nota", methods=['GET', 'POST'])
+def añadir_nota():
+    form = AñadirNotaForm()
+    if form.is_submitted():
+        # logica
+        # form.password.data
+        # form.username.data
+        return redirect(url_for('añadir_nota'))
+    return render_template('admin/añadir_nota/añadir_nota.html', form=form)
+
+@app.route("/eliminar_estudiante", methods=['GET', 'POST'])
+def eliminar_estudiante():
+    form = EliminarEstudianteForm()
+    if form.is_submitted():
+        # logica
+        # form.password.data
+        # form.username.data
+        return redirect(url_for('eliminar_estudiante'))
+    return render_template('admin/eliminar_estudiante/eliminar_estudiante.html', form=form)
+
+@app.route("/eliminar_nota", methods=['GET', 'POST'])
+def eliminar_nota():
+    form = EliminarNotaForm()
+    if form.is_submitted():
+        # logica
+        # form.password.data
+        # form.username.data
+        return redirect(url_for('eliminar_nota'))
+    return render_template('admin/eliminar_nota/eliminar_nota.html', form=form)
 
 @app.route("/lista_estudiantes")
 def lista_estudiantes():
